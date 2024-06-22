e714278638d816e5bc9b7e1c06be76a433876d10
Luffy20241 <168181882+Luffy20241@users.noreply.github.com>
2024-06-23 05:51:54 +0600

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://slac:slact@fsub.qnbs3sa.mongodb.net/?retryWrites=true&w=majority&appName=Fsub"
 DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "vnshortener.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "IbKMaU0BVOAAjdwtcRommfxgb2ntF4LAKSjKchYx")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "0")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "0")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/Realm_Of_Zoro/8")
